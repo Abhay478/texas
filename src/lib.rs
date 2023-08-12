82c14b47ee0ea3a660d1c4ddc89c4f4cf5234c2d
Abhay478 <96719488+Abhay478@users.noreply.github.com>
2023-08-12 17:53:46 +0530

    gitdo:?

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//!
+//! Well, hello there.
+//! 
+//! So this crate aims to provide convenient ways of programmatically generating a latex file.
+//! 
+//! If you want to just convert from some other file format WITHOUT any processing, I recommend `pandoc`.
+//! 
+//! I'll slowly and steadily begin adding more features, and my goal is to be able to cover most standard use-cases of Latex.
+//! If you want any feature, raise an issue! I will see it, and while I may not reply (scatterbrained, much?) I will fix it.
+//! 
+//! A shout-out to another crate, `tex-rs`. A few of the design choices I made are based on this crate.
+//! Mine's still better, though. 😉
+//! 
+
+
+
 use std::error::Error;
 
 /// Bunch of From<>s, they feel like they might be useful
